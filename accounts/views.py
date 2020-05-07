1f503164d0cb8fc04d76bfd2bf4e3f9bb87c741f
Robert Davies <rob.davies1991@gmail.com>
2020-05-07 10:04:48 +0100

    bootstraped template and forms

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required()
+def userControlPanel(request):
+
+    user = User.objects.get(email=request.user.email)
+
+    return render(request, "ucp.html", {"profile": user})
