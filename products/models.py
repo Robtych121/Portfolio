6827633e9058de09b066e4fbb721d5ddc79fdbde
Robert Davies <rob.davies1991@gmail.com>
2020-05-10 14:23:47 +0100

    added S3 file storage support

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     points = models.IntegerField(blank=False, default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images', default='')
 
     def __str__(self):
         return self.name
