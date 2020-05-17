e67d4c773dbe73cd84d3cd8b57bf83529eac7ffa
Robert Davies <rob.davies1991@gmail.com>
2020-05-17 14:23:07 +0100

    added go to top button

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import mainportfolio
+from .views import mainportfolio, view_portfolio
 
 urlpatterns = [
-    path('', mainportfolio, name="mainportfolio")
+    path('', mainportfolio, name="mainportfolio"),
+    path('view/<int:id>/', view_portfolio, name="view_portfolio")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -8,3 +8,9 @@ def mainportfolio(request):
     categories = Portfolios.objects.values_list('category', flat=True).distinct()
 
     return render(request, 'main_portfolio.html', {'portfolios': portfolios, 'categories': categories})
+
+
+def view_portfolio(request, id):
+    portfolio = Portfolios.objects.get(id=id)
+
+    return render(request, 'view_portfolio.html', {'portfolio': portfolio})
